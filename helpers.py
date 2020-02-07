c4c3c4473ea51cba266e46716da04b09c03fde65
mkhayralla <gonfreecs303@gmail.com>
2020-02-07 03:29:48 +0200

    solved quoted url bug

--- helpers.py
+++ helpers.py
@@ -1,5 +1,7 @@
 import os
 
+#url decoding
+from urllib.parse import unquote
 
 def initiate_app():
     if ('downloads' not in os.listdir()):
@@ -8,6 +10,7 @@ def initiate_app():
 
 def generate_img_url(url):
     result = url[url.find('&url=')+5:] if url.find('&url=') > -1 else url
+    result = unquote(result)
     extention = result.rfind('.')
     try:
         return result[0:extention+4]
